45c56379f85d670342aa4bc9028a159848f17bc2
Karyna Yen <kyen@hubspot.com>
2025-10-11 15:49:50 -0400

    fixes

--- .next/server/chunks/ssr/[root-of-the-server]__ef8f5c39._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__ef8f5c39._.js
@@ -273,7 +273,7 @@ function BooleanVariables() {
     const true2 = true1 || false1;
     const true3 = !false2;
     const true4 = numberVariable === 123; // always use === not ==
-    const true5 = floatingPointNumber !== 321.432;
+    const true5 = floatingPointNumber - 321.432 === 0; // Can't directly compare floating point numbers
     const false3 = numberVariable < 100;
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
         id: "wd-boolean-variables",
--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from 'next/navigation';
 import Lab1 from './Lab1/page';
 
 export default function Labs() {
-  return <Lab1 />;
-  // redirect("/Labs/Lab1");
+  // return <Lab1 />;
+  redirect('/Labs/Lab1');
 }
