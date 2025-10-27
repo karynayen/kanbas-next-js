032528b5757e740e2e5fd6f6ed80a50ea37eedab
Karyna Yen <kyen@hubspot.com>
2025-10-26 20:38:35 -0400

    adding state to canvas reducer

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import Link from 'next/link';
 import * as db from '../Database';
 import {
@@ -9,12 +11,75 @@ import {
   CardTitle,
   CardText,
   Button,
+  FormControl,
 } from 'react-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
 export default function Dashboard() {
-  const courses = db.courses;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [course, setCourse] = useState<any>({
+    _id: '0',
+    name: 'New Course',
+    number: 'New Number',
+    startDate: '2023-09-10',
+    endDate: '2023-12-15',
+    image: '/images/reactjs.jpg',
+    description: 'New Description',
+  });
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: uuidv4() };
+    setCourses([...courses, newCourse]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>
+        New Course
+        <button
+          className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse}
+        >
+          {' '}
+          Add{' '}
+        </button>
+        <button
+          className="btn btn-warning float-end me-2"
+          onClick={updateCourse}
+          id="wd-update-course-click"
+        >
+          Update{' '}
+        </button>
+      </h5>
+      <br />
+      <FormControl
+        value={course.name}
+        className="mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <FormControl
+        as="textarea"
+        value={course.description}
+        rows={3}
+        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+      />
+      <hr />
       <h2 id="wd-dashboard-published">
         Published Courses ({courses.length})
       </h2>{' '}
@@ -49,6 +114,26 @@ export default function Dashboard() {
                       {course.description}{' '}
                     </CardText>
                     <Button variant="primary"> Go </Button>
+                    <button
+                      onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }}
+                      className="btn btn-danger float-end"
+                      id="wd-delete-course-click"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      id="wd-edit-course-click"
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                      className="btn btn-warning me-2 float-end"
+                    >
+                      Edit
+                    </button>
                   </CardBody>
                 </Link>
               </Card>
