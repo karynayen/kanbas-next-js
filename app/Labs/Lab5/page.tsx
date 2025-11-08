a0688697fc390a62e7f182b167e49f1a36b72312
Karyna Yen <kyen@hubspot.com>
2025-11-07 22:29:30 -0500

    up to 5.2.3.4 On Your Own

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -2,6 +2,7 @@
 import EnvironmentVariables from './EnvironmentVariables';
 import PathParameters from './PathParameters';
 import QueryParameters from './QueryParameters';
+import WorkingWithObjects from './WorkingWithObjects';
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 
 export default function Lab5() {
@@ -17,6 +18,7 @@ export default function Lab5() {
       <EnvironmentVariables />
       <PathParameters />
       <QueryParameters />
+      <WorkingWithObjects />
     </div>
   );
 }
